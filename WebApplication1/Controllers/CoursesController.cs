3321e1329eaeff2f441feaf4c50ad2e9b54a3c7b
HP <HP@LAPTOP-M04EJP5H>
2023-03-15 01:58:15 +0700

    Add project files.

---
+++ WebApplication1/Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        // GET: Courses
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+
+            };
+            return View(viewModel);
+        }
+    }
+}
